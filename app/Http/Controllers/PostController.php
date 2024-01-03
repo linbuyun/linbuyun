fab68bd62d5fdee5fe8b9f682d67fba608f7bd99
linbuyun <GitHub>
2024-01-04 00:07:03 +0900

    1.4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,17 +12,22 @@ class PostController extends Controller
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
     public function show(Post $post)
-   {
+    {
         return view('posts/show')->with(['post' => $post]);
-   }
+    }
     public function create()
     {
         return view('posts/create');
-    }
+    }  
     public function store(Request $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/post/' . $post->id);
     }
-}
+} 
+ 
+
+
+
+     
