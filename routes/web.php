67453eab74586361dd2d36282f355de796e7f0e7
linbuyun <GitHub>
2024-01-06 19:07:09 +0900

    1.6 19:07

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,6 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
-Route::get('/post/{post}/edit',[PostController::class, 'edit']);
+Route::get('/posts/{post}/edit',[PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
