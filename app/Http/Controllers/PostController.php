e5994bd96e500beaea2f63728616f1b3c527539d
linbuyun <GitHub>
2024-01-10 18:59:58 +0900

    1.10 18:59

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Models\Category;
+
 
 class PostController extends Controller
 {
@@ -16,11 +18,10 @@ class PostController extends Controller
     {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts/create');
+    return view('posts.create')->with(['categories' => $category->get()]);
     }
-    
     public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
